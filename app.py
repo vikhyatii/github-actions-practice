fb103419d98d44cf001fe82c29242eb250df7752
Vikhyati Harisinghani <92918964+vikhyatii@users.noreply.github.com>
2026-06-26 11:19:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ num = 29
 flag = False
 
 if num == 0 or num == 1:
-    print(num, "is not a prime number")
+print(num, "is not a prime number")
 elif num > 1:
     # check for factors
     for i in range(2, num):
